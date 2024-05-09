2a67f98bdc60648116a8d35e049a79b52fce4a50
PotatoCodez127 <86016076+PotatoCodez127@users.noreply.github.com>
2024-05-09 18:28:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+import smtplib
+
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
+
+app = Flask(__name__)
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+EMAIL = getenv('EMAIL')
+PASSWORD = getenv('PASSWORD')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        send_email(name=name, email=email, message=message)
+        return redirect(url_for('home'))
+    else:
+        return render_template('index.html')
+
+
+def send_email(name, email, message):
+    email_message = f"Subject:WEBSITE EMAIL\n\nName: {name}\nEmail: {email}\n{message}"
+    with smtplib.SMTP(host=EMAIL_HOST, port=EMAIL_PORT) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            EMAIL,
+            EMAIL,
+            email_message
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
